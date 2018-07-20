62984a57e5c82f09ebc8e80e17fba1c93a72d550
richtier <rikatee@gmail.com>
2018-07-21 00:26:27 +0100

    Increase the size of the status text

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements():
 
 setup(
     name='alexa_browser_client',
-    version='1.0.1',
+    version='1.1.0',
     url='https://github.com/richtier/alexa-browser-client',
     license='MIT',
     author='Richard Tier',
