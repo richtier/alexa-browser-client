8877356cb46056ebc65e9de04ebf5bb8f6c134c3
richtier <rikatee@gmail.com>
2018-09-13 22:34:27 +0100

    Use markdown for readme

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='alexa_browser_client',
-    version='1.3.0',
+    version='1.4.0',
     url='https://github.com/richtier/alexa-browser-client',
     license='MIT',
     author='Richard Tier',
     description='Alexa client in your browser. Django app.',
     packages=find_packages(exclude=["tests.*", "tests"]),
-    long_description=open('docs/README.rst').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=[
         'channels<2.0.0',
         'Django>=1.11.1<2.0.0',
         'avs-client>=0.7.0<1.0.0',
-        'command-lifecycle>=2.0.0<3.0.0',
+        'command-lifecycle>=2.2.0<3.0.0',
         'requests>=2.19.1<3.0.0',
     ],
     classifiers=[
