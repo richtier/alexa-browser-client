2ad68e459d95045920d7e2acd249907e88c41a72
richtier <rikatee@gmail.com>
2018-12-31 20:46:01 +0000

    Improve packaging

--- Makefile
+++ Makefile
@@ -14,15 +14,24 @@ test:
 		--verbose
 
 
-publish_test:
+build:
 	rm -rf build
 	rm -rf dist
 	python setup.py bdist_wheel
+
+
+publish_test:
+	make build
 	twine upload -r pypitest dist/*
 
 
 publish:
+	make build
 	twine upload dist/*
+
+
+test_requirements:
+	pip install -e .[test]
 
 
 demo:
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alexa_browser_client',
-    version='3.0.0',
+    version='3.1.0',
     url='https://github.com/richtier/alexa-browser-client',
     license='MIT',
     author='Richard Tier',
@@ -16,10 +16,26 @@ setup(
         'channels>=2.1.4,<3.0.0',
         'channels_redis>=2.1.1,<3.0.0',
         'Django>=2.1.0,<3.0.0',
-        'avs-client>=0.7.1,<1.0.0',
+        'avs-client>=1.1.0,<2.0.0',
         'command-lifecycle>=2.0.0,<3.0.0',
         'requests>=2.19.1,<3.0.0',
     ],
+    extras_require={
+        'test': [
+            'pytest==3.2.3',
+            'pytest-cov==2.5.1',
+            'pytest-django==3.1.2',
+            'pytest-sugar==0.9.0',
+            'flake8==3.4.0',
+            'codecov==2.0.9',
+            'requests_mock==1.3.0',
+            'django-environ==0.4.3',
+            'pytest-asyncio==0.9.0',
+            'twine>=1.11.0,<2.0.0',
+            'wheel>=0.31.0,<1.0.0',
+            'setuptools>=38.6.0,<39.0.0',
+        ]
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
